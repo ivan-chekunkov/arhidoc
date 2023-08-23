d73f485e0e4296dd91fe9d6be28a9ecbb2618da8
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-08-23 22:48:56 +0300

    correct utilites

--- arhidoc_convert_doc_to_docx.py
+++ arhidoc_convert_doc_to_docx.py
@@ -7,3 +7,12 @@ from doc2docx import convert as doc2docx_convert
 def _iterdir(path: Path) -> list[Path]:
     """Получение списка файлов"""
     return [x for x in path.iterdir() if x.is_file()]
+
+
+def validate_docx(path_file: Path, prefix: str, level: int = 1) -> None:
+    """Валидация содержимого документов их данным согласно выгрузке из"""
+    pass
+
+
+if __name__ == '__main__':
+    validate_docx(Path('D13.csv'), prefix='Л', level=1)
