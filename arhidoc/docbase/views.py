984d9a757e1243d6d98b4a71b80d7517d16eaca8
ivan-chekunkov <chekunkov.ivan@yandex.ru>
2023-08-29 10:55:58 +0300

    add paginator

--- arhidoc/docbase/views.py
+++ arhidoc/docbase/views.py
@@ -3,19 +3,20 @@ from django.http import HttpResponse, Http404
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.core.files.storage import FileSystemStorage
+from django.core.paginator import Paginator
 
 from .models import Doc, Category
 from .forms import DocumentForm
 
 
-def index(request):
+def index2(request):
     template = "docbase/index.html"
     title = "Документы"
     docs = Doc.objects.order_by("-pub_create")
     context = {"title": title, "text": docs}
     return render(request, template, context)
 
-def create_docs(request):
+def category(request):
     template = "docbase/category.html"
     title = "Категории"
     category = Category.objects.all()
@@ -46,3 +47,10 @@ def download(request, path):
                 os.path.basename(file_path)
             return response
     raise Http404
+
+def index(request):
+    contact_list = Doc.objects.order_by("-pub_create")
+    paginator = Paginator(contact_list, 25)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, "docbase/list.html", {"page_obj": page_obj})
