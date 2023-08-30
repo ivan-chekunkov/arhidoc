c3071a6457f7cb6f6338adb38041e0b2bb391527
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-08-30 22:17:46 +0300

    del comment

--- arhidoc/arhidoc/urls.py
+++ arhidoc/arhidoc/urls.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from docbase.views import model_form_upload
 
 urlpatterns = [
     path("", include("docbase.urls", namespace="docbase")),
     path("admin/", admin.site.urls),
-    re_path(r'^uploads/form/$', model_form_upload, name='model_form_upload'),
 ]
 
 if settings.DEBUG:
