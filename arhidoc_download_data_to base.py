b8f4b29e9a9c4b37676ad61a530d6d80d30e1aa9
ivan-chekunkov <chekunkov.ivan@yandex.ru>
2023-09-03 11:25:21 +0300

    correct utilites

--- arhidoc_download_data_to base.py
+++ arhidoc_download_data_to base.py
@@ -1,4 +1,5 @@
 import sqlite3
+from pathlib import Path
 
 
 conn = sqlite3.connect('arhidoc\\db.sqlite3')
@@ -22,5 +23,20 @@ def create_db():
     """)
     conn.commit()
 
+def _iterdir(path: Path) -> list[Path]:
+    """Получение списка файлов"""
+    return [x for x in path.iterdir() if x.is_file()]
+
+
+def get_base_dir():
+    return Path(__file__).parent.joinpath('DOCS_FOR_DOWNLOAD')
+
+def check_db():
+    sqlite_insert_with_param = """SELECT dd.* 
+                                FROM docbase_doc dd """
+    cur.execute(sqlite_insert_with_param)
+    conn.commit()
+
 if __name__ == '__main__':
     create_db()
+    check_db()
